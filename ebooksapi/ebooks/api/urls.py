c96aa2bb258610d5d64c45ab2f87b18626c7ef60
Jorge Cabral <jorgect99@gmail.com>
2021-01-13 22:27:26 -0700

    Permissions System

--- ebooksapi/ebooks/api/urls.py
+++ ebooksapi/ebooks/api/urls.py
@@ -3,8 +3,8 @@ from ebooks.api.views import EbookListCreateAPIView, EbookDetailAPIView, ReviewC
 
 urlpatterns  = [
     path("ebooks/", EbookListCreateAPIView.as_view(), name = "ebook-list"),
-    path("ebooks/<int:pk>", EbookDetailAPIView.as_view(), name = "ebook-detail"),
+    path("ebooks/<int:pk>/", EbookDetailAPIView.as_view(), name = "ebook-detail"),
 
     path("ebooks/<int:ebook_pk>/review/", ReviewCreateAPIView.as_view(), name = "ebook-review"),
-    path("reviews/<int:pk>", ReviewDetailAPIView.as_view(), name = "review-detail"),
+    path("reviews/<int:pk>/", ReviewDetailAPIView.as_view(), name = "review-detail"),
 ]
